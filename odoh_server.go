f13424df2ae0c43fd6c44c792df11670dea5179f
Christopher Wood <caw@heapingbits.net>
2019-10-03 06:59:12 -0700

    WIP logging.

--- odoh_server.go
+++ odoh_server.go
@@ -1,37 +1,56 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"encoding/hex"
+	"fmt"
+	"github.com/bifurcation/hpke"
+	"github.com/chris-wood/odoh"
+	"log"
 	"net/http"
 	"time"
-	"github.com/chris-wood/odoh"
-	"github.com/bifurcation/hpke"
 )
 
+const (
+	// HPKE constants
+	kemID          = hpke.DHKEM_X25519
+	kdfID          = hpke.KDF_HKDF_SHA256
+	aeadID         = hpke.AEAD_AESGCM128
+	publicKeyBytes = "85023a65b2c505cd2e92e2c427ef69df8aa8d0f18081a8090b159aafa6001413"
+	skRm           = "c2dd775b50210ad308e43b3dd45c5eabc085df1398c8dce6501598c1575dbd21"
 
-func handle(w http.ResponseWriter, r *http.Request) {
-	log.Printf("%s Handling %s\n", r.Method, r.URL.Path)
-	// TODO(caw): dump info about the endpoints instead
-	fmt.Fprint(w, "ODOH, try /dns-query instead!")
+	// DNS constants
+	nameServer = "1.1.1.1:53"
+
+	// HTTP constants
+	proxyEndpoint  = "/dns-query/proxy"
+	targetEndpoint = "/dns-query"
+	healthEndpoint = "/health"
+)
+
+type Server struct {
+	odohServer odoh.Server
+	endpoints  map[string]string
 }
 
-func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+func (s Server) handle(w http.ResponseWriter, r *http.Request) {
+	log.Printf("%s Handling %s\n", r.Method, r.URL.Path)
+	fmt.Fprint(w, "ODOH service")
+	fmt.Fprint(w, "----------------")
+	fmt.Fprintf(w, "Proxy endpoint: https://%s:%s/%s\n", r.URL.Hostname(), r.URL.Port(), s.endpoints[proxyEndpoint])
+	fmt.Fprintf(w, "Target endpoint: https://%s:%s/%s\n", r.URL.Hostname(), r.URL.Port(), s.endpoints[targetEndpoint])
+	fmt.Fprint(w, "----------------")
+
+	fmt.Fprint(w, "Tail logs")
+
+}
+
+func (s Server) healthCheckHandler(w http.ResponseWriter, r *http.Request) {
 	log.Printf("%s Handling %s\n", r.Method, r.URL.Path)
 	fmt.Fprint(w, "ok")
 }
 
-const (
-	kemID = hpke.DHKEM_X25519
-	kdfID = hpke.KDF_HKDF_SHA256
-	aeadID = hpke.AEAD_AESGCM128
-	pkRm = "85023a65b2c505cd2e92e2c427ef69df8aa8d0f18081a8090b159aafa6001413"
-	skRm = "c2dd775b50210ad308e43b3dd45c5eabc085df1398c8dce6501598c1575dbd21"
-)
-
 func main() {
-	publicKeyBytes, err := hex.DecodeString(pkRm)
+	publicKeyBytes, err := hex.DecodeString(publicKeyBytes)
 	if err != nil {
 		log.Fatal("Failed to decode public key. Exiting now.")
 	}
@@ -41,22 +60,37 @@ func main() {
 		log.Fatal("Failed to decode private key. Exiting now.")
 	}
 
-	privateKey, err  := odoh.CreatePrivateKeyDeterministic(kemID, kdfID, aeadID, publicKeyBytes, secretKeyBytes)
+	privateKey, err := odoh.CreatePrivateKeyDeterministic(kemID, kdfID, aeadID, publicKeyBytes, secretKeyBytes)
 	if err != nil {
 		log.Fatal("Failed to create a private key. Exiting now.")
 	}
 
-	server := odoh.Server{
+	// TODO(caw): turn this into an ODOH object that collects rolling logs as formatted strings
+	var buf bytes.Buffer
+	logger := log.New(&buf, "ODOH: ", log.LstdFlags)
+
+	odohServer := odoh.Server{
 		Verbose:    true,
+		Logger:     logger,
 		Timeout:    2500 * time.Millisecond,
-		Nameserver: "1.1.1.1:53",
+		Nameserver: nameServer,
 		PrivateKey: privateKey,
 	}
 
-	http.HandleFunc("/dns-query/proxy", server.ProxyHandler)
-	http.HandleFunc("/dns-query", server.QueryHandler)
-	http.HandleFunc("/health", healthCheckHandler)
-	http.HandleFunc("/", handle)
+	endpoints := make(map[string]string)
+	endpoints["Proxy"] = proxyEndpoint
+	endpoints["Target"] = targetEndpoint
+	endpoints["Health"] = healthEndpoint
+
+	server := Server{
+		odohServer: odohServer,
+		endpoints:  endpoints,
+	}
+
+	http.HandleFunc(proxyEndpoint, server.odohServer.ProxyHandler)
+	http.HandleFunc(targetEndpoint, server.odohServer.QueryHandler)
+	http.HandleFunc(healthEndpoint, server.healthCheckHandler)
+	http.HandleFunc("/", server.handle)
 
 	log.Print("Listening on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
